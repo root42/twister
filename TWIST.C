255dc1f6537ec3dda042bcfbba4463f3f3600f3c
Arne Schmitz <arne.schmitz@gmx.net>
2020-09-13 23:20:02 +0200

    version in the let's code video

--- TWIST.C
+++ TWIST.C
@@ -19,10 +19,6 @@ byte far *framebuf;
 #define SETPIX(x,y,c) *(framebuf + (dword)SCREEN_WIDTH * (y) + (x)) = c
 #define GETPIX(x,y) *(framebuf + (dword)SCREEN_WIDTH * (y) + (x))
 
-#ifndef M_PI
-#define M_PI 3.14159
-#endif
-
 unsigned char SIN4096[4096];
 
 void init_sin()
@@ -35,39 +31,38 @@ void init_sin()
 
 void hline( word x0, word x1, word y, byte col )
 {
-  int i;
+  int x;
   if( y >= SCREEN_HEIGHT ) {
     return;
   }
-  for( i = x0; i < x1 && i < SCREEN_WIDTH; ++i ) {
-    SETPIX( i, y, col );
+  for( x = x0; x < x1 && x < SCREEN_WIDTH; ++x ) {
+    SETPIX( x, y, col );
     col++;
   }
 }
 
 void draw_twist( word x0, word y0, word w, word h, word t )
 {
-  word a;
-  word y, x1, x2, x3, x4, xm;
+  word a, y, x1, x2, x3, x4, xm;
 
-  for( y = y0; y < y0 + h; ++y ) {
-    a  =       SIN4096[(t * 16 + y * 2) % 4096] * (w >> 2);
-    xm = x0 + (SIN4096[(t * 16 + y * 8) % 4096] >> 3);
+  for( y = y0; y < y0 + h; y++ ) {
+    a  =       SIN4096[((t << 4) + (y << 1)) % 4096] * (w >> 2);
+    xm = x0 + (SIN4096[((t << 4) + (y << 3)) % 4096] >> 2);
     x1 = xm + (SIN4096[(a +    0) % 4096] >> 2);
     x2 = xm + (SIN4096[(a + 1024) % 4096] >> 2);
     x3 = xm + (SIN4096[(a + 2048) % 4096] >> 2);
     x4 = xm + (SIN4096[(a + 3072) % 4096] >> 2);
-    if(x1 < x2) {
-      hline( x1, x2, y, 33);
+    if( x1 < x2 ) {
+      hline( x1, x2, y, 33 );
     }
-    if(x2 < x3) {
-      hline( x2, x3, y, 49);
+    if( x2 < x3 ) {
+      hline( x2, x3, y, 49 );
     }
-    if(x3 < x4) {
-      hline( x3, x4, y, 65);
+    if( x3 < x4 ) {
+      hline( x3, x4, y, 65 );
     }
-    if(x4 < x1) {
-      hline( x4, x1, y, 81);
+    if( x4 < x1 ) {
+      hline( x4, x1, y, 81 );
     }
   }
 }
@@ -76,7 +71,6 @@ int main()
 {
   int i;
   word frame = 0;
-  word front, back;
 
   randomize();
   frame = random(1024);
@@ -86,13 +80,11 @@ int main()
   set_palette( fire_pal );
   framebuf = malloc( SCREEN_WIDTH*SCREEN_HEIGHT );
   if( framebuf == NULL ) {
-    printf("not enough memory\n");
+    printf("not enough memory!\n");
     return 1;
   }
-  memset( framebuf, 0x00, SCREEN_WIDTH*SCREEN_HEIGHT );
 
-  while(!kbhit())
-  {
+  while( !kbhit() ) {
     memset( framebuf, 0x00, SCREEN_WIDTH*SCREEN_HEIGHT );
     draw_twist( 100, 0, 128, 200, frame );
     wait_for_retrace();
